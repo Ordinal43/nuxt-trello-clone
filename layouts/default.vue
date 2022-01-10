c54844b5445dc9a292eb9dcc498ce7a6ae9b15b0
Bryan Ramaputra <ramaputra.bryan@gmail.com>
2022-01-09 16:52:18 -0800

    Add app bar for authenticated page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,70 @@
 <template>
   <v-app>
+    <v-app-bar
+      v-if="$store.getters.getUser"
+      app
+      dense
+    >
+      <v-toolbar-title>Brello</v-toolbar-title>
+      <v-spacer />
+
+      <v-menu
+        bottom
+        left
+        offset-y
+        :close-on-content-click="false"
+      >
+        <template
+          #activator="{ on, attrs }"
+        >
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-account-circle</v-icon>
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-list>
+            <v-list-item>
+              <v-list-item-avatar>
+                <img
+                  src="https://cdn.vuetifyjs.com/images/john.jpg"
+                  alt="John"
+                >
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title>[Your name here]</v-list-item-title>
+                <v-list-item-subtitle>{{ $store.getters.getUser.email }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+
+          <v-divider />
+
+          <v-list>
+            <v-list-item
+              nuxt
+              to="/auth/applogout"
+            >
+              <v-list-item-icon>
+                <v-icon>
+                  mdi-logout
+                </v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>
+                  Logout
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
+    </v-app-bar>
     <v-main>
       <Nuxt />
     </v-main>
