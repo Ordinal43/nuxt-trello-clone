8610d0d63d116b11b0f26920015dc7da6bda4e37
Bryan Ramaputra <ramaputra.bryan@gmail.com>
2022-01-14 17:27:40 -0800

    Add Vue filter for current date format

--- plugins/filters.js
+++ plugins/filters.js
@@ -8,3 +8,8 @@ Vue.filter('formatFromNow', (value) => {
   if (!value) { return '' }
   return dayjs(value).fromNow()
 })
+
+Vue.filter('formatDate', (value) => {
+  if (!value) { return '' }
+  return dayjs(value).format('MMM D, YYYY')
+})
