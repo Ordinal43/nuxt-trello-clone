fbc4d99d5725ca65da23578adfe8521ea37b27a0
Bryan Ramaputra <ramaputra.bryan@gmail.com>
2022-01-29 01:15:36 -0800

    Add mixin to check for due date status

--- mixins/vue-mixins.js
+++ mixins/vue-mixins.js
@@ -1,3 +1,5 @@
+import dayjs from '@/utils/dayjs.utils'
+
 export const mixinTextArea = {
   methods: {
     mixin_resizeTextarea ({ target }) {
@@ -6,6 +8,28 @@ export const mixinTextArea = {
        */
       target.style.height = '0px'
       target.style.height = (target.scrollHeight) + 'px'
+    }
+  }
+}
+
+export const mixinDate = {
+  methods: {
+    /**
+     * get date status in array form
+     */
+    mixin_getDateDueStatus (dateStr, dueSoonMessage = 'due soon', overdueMessage = 'overdue') {
+      const diff = dayjs(dateStr).diff(dayjs(), 'second')
+      let color, message
+      if (diff <= 0) {
+        color = 'red lighten-1'
+        message = overdueMessage
+      } else if (diff < 86400) {
+        color = 'amber lighten-1'
+        message = dueSoonMessage
+      } else {
+        return null
+      }
+      return { color, message }
     }
   }
 }
