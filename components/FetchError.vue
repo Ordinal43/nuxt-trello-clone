865a90096472bf2816507dcf5a6a7203252dcbb5
Bryan Ramaputra <ramaputra.bryan@gmail.com>
2022-02-06 04:29:57 -0800

    Add custom components for displaying fetch states

---
+++ components/FetchError.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="fill-height d-flex align-center justify-center">
+    Fetching Error!
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ components/FetchPending.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="fill-height d-flex align-center justify-center">
+    Fetching...
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
