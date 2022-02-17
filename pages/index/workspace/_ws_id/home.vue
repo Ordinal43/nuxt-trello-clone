dfb86444ce855f2e579da3daa478fc213ee503b9
Bryan Ramaputra <ramaputra.bryan@gmail.com>
2022-02-17 06:27:00 -0800

    Add content for workspace detail on home page

--- pages/index/workspace/_ws_id/home.vue
+++ pages/index/workspace/_ws_id/home.vue
@@ -1,5 +1,15 @@
 <template>
-  <WorkspaceBoards />
+  <div>
+    <div class="mb-5 d-flex align-center">
+      <v-icon>
+        mdi-account-outline
+      </v-icon>
+      <h4 class="ml-4">
+        Workspace boards
+      </h4>
+    </div>
+    <WorkspaceBoards />
+  </div>
 </template>
 
 <script>
